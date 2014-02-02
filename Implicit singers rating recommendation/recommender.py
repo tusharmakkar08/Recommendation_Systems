a0d5e16a68635269e552c4cb0f02e06da0e90c10
Tushar Makkar <tusharmakkar08@gmail.com>
2014-02-02 15:16:27 +0530

    Slope One implemented

--- Implicit singers rating recommendation/recommender.py
+++ Implicit singers rating recommendation/recommender.py
@@ -10,9 +10,14 @@ class recommender:
       # The following two variables are used for Slope One
 		self.frequencies = {}
 		self.deviations = {}
+		self.productid2name = {}
 		if type(data).__name__ == 'dict':
 			self.data = data
-	
+	def convertProductID2name(self, id):
+		if id in self.productid2name:
+			return self.productid2name[id]
+		else:
+			return id
 	def computeDeviations(self):
 		for ratings in self.data.values():
 			for (item,rating) in ratings.items():
@@ -31,6 +36,24 @@ class recommender:
 			for item2 in ratings:
 				ratings[item2]/=self.frequencies[item][item2]
 
+	def slopeOneRecommendations(self,userRatings):
+		recommendations={}
+		frequencies={}
+		for (userItem,userRating) in userRatings.items():
+			for (diffItem,diffRatings) in self.deviations.items():
+				if diffItem not in userRatings and userItem in self.deviations[diffItem]:
+					freq=self.frequencies[diffItem][userItem]
+					recommendations.setdefault(diffItem,0.0)
+					frequencies.setdefault(diffItem,0)
+				
+					recommendations[diffItem]+=(diffRatings[userItem]+userRating)*freq
+					frequencies[diffItem]+=freq
+		recommendations =[(self.convertProductID2name(k),v / frequencies[k])for (k, v) in recommendations.items()]
+		recommendations.sort(key=lambda artistTuple: artistTuple[1],reverse = True)
+		return recommendations
+		
+					
 #r = recommender(users2)
 #r.computeDeviations()
-#print r.deviations
+#g = users2['Ben']
+#print r.slopeOneRecommendations(g)
